f9f575386a004e51f1d594e48e3bdbb9633a72db
akash patil <akashpatil.ap1610@gmail.com>
2026-01-02 01:19:07 +0530

    redis issue fixed

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,12 +1,19 @@
-const { createClient }  = require('redis');
+const { createClient } = require('redis');
 
 const redisClient = createClient({
     username: 'default',
-    password: process.env.REDIS_PASS,
+    password: 'JG8HI4sTxUny05hyWGD8Lq3hRhHjl7eQ',
     socket: {
-        host: 'redis-19934.c212.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 19934
+        host: 'redis-18466.c326.us-east-1-3.ec2.cloud.redislabs.com',
+        port: 18466 
     }
 });
+
+
+// Add error handling
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+// IMPORTANT: Connect before using
+
 
 module.exports = redisClient;
